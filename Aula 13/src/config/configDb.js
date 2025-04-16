1a78bfe8b3c200cbe1137f165c9bfa8b2838c974
BielDias2501 <gabrieldiascl@gmail.com>
2025-04-16 17:25:06 -0300

    add: adicionei o back end atualizado

--- Aula 13/src/config/configDb.js
+++ Aula 13/src/config/configDb.js
@@ -12,3 +12,11 @@ const sequelize = new Sequelize(
 
 module.exports = sequelize
 
+// DEV_DB_PASSWORD=BemVindo!
+// DEV_DB_DIALECT=postgres
+// DEV_DB_USER=postgres
+// DEV_DB_HOST=localhost
+// DEV_DB_NOME=sistema_academico
+// DEV_DB_PORTA=5432
+// PORTA=3000
+// NODE_ENV=development
